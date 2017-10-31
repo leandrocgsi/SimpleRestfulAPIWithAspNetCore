192f4025588658dd9de0259e2698e41f7aa2b5ad
leandrocgsi <leandrocgsi@gmail.com>
2017-10-31 10:41:54 -0200

    Adding Swagger support

--- SimpleRestfulAPIWithAspNetCore-v03/SimpleRestfulAPIWithAspNetCore/Startup.cs
+++ SimpleRestfulAPIWithAspNetCore-v03/SimpleRestfulAPIWithAspNetCore/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using SimpleRestfulAPIWithAspNetCore.Services;
 using SimpleRestfulAPIWithAspNetCore.Services.Implementations;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace SimpleRestfulAPIWithAspNetCore
 {
@@ -27,6 +23,12 @@ namespace SimpleRestfulAPIWithAspNetCore
         {
             services.AddMvc();
 
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My Simple RESTful API with ASP.NET Core", Version = "v1" });
+            });
+
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImpl>();
         }
@@ -39,7 +41,26 @@ namespace SimpleRestfulAPIWithAspNetCore
                 app.UseDeveloperExceptionPage();
             }
 
+            // Habilita a disponiblilização pela API da documentação do Swagger como um endpoint JSON.
+            app.UseSwagger();
+
+            // Habilita a disponiblilização pela API pela disponibilização do swagger-ui contendo
+            // (HTML, JS, CSS, etc.), specificando o endpoint JSON do Swagger.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            // Define a página do Swagger como página inicial
+            // Caso não funcionar verificar se o arquivo
+            // launchSettings não está sobrescrevendo essa regra
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
             app.UseMvc();
         }
+
+        //SEE: https://docs.microsoft.com/pt-br/aspnet/core/tutorials/web-api-help-pages-using-swagger?tabs=visual-studio
     }
 }
