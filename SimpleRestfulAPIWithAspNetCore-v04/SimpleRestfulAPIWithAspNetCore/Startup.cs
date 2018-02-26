a15761b2e7fd7faf6225fda65a3fd60dd4a1c22d
leandrocgsi <leandrocgsi@gmail.com>
2018-02-26 09:21:55 -0300

    Connecting to database

---
+++ SimpleRestfulAPIWithAspNetCore-v04/SimpleRestfulAPIWithAspNetCore/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleRestfulAPIWithAspNetCore.Services;
+using SimpleRestfulAPIWithAspNetCore.Services.Implementations;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace SimpleRestfulAPIWithAspNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My Simple RESTful API with ASP.NET Core", Version = "v1" });
+            });
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Habilita a disponiblilização pela API da documentação do Swagger como um endpoint JSON.
+            app.UseSwagger();
+
+            // Habilita a disponiblilização pela API pela disponibilização do swagger-ui contendo
+            // (HTML, JS, CSS, etc.), specificando o endpoint JSON do Swagger.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            // Define a página do Swagger como página inicial
+            // Caso não funcionar verificar se o arquivo
+            // launchSettings não está sobrescrevendo essa regra
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
+            app.UseMvc();
+        }
+
+        //SEE: https://docs.microsoft.com/pt-br/aspnet/core/tutorials/web-api-help-pages-using-swagger?tabs=visual-studio
+    }
+}
