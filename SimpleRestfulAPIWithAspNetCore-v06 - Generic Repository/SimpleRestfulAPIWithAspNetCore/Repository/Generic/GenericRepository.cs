8c2527c9b40306a6c931a5ca6816e1b1c932f55d
leandrocgsi <leandrocgsi@gmail.com>
2018-03-09 10:13:12 -0300

    Implementing generic repository

---
+++ SimpleRestfulAPIWithAspNetCore-v06 - Generic Repository/SimpleRestfulAPIWithAspNetCore/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SimpleRestfulAPIWithAspNetCore.Models.Entities.Base;
+using SimpleRestfulAPIWithAspNetCore.Models.Entities.Context;
+
+namespace SimpleRestfulAPIWithAspNetCore.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly MySQLContext _context;
+        private DbSet<T> dataSet;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            dataSet = _context.Set<T>();
+        }
+
+        public T Add(T item)
+        {
+            try
+            {
+                dataSet.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public T Update(T item)
+        {
+            var result = dataSet.SingleOrDefault(i => i.Id == item.Id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return result;
+        }
+
+        public void Remove(object id)
+        {
+            T result = dataSet.SingleOrDefault(i => i.Id.Equals(id));
+            try
+            {
+                dataSet.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Remove(T entityToDelete)
+        {
+            try
+            { 
+                if (_context.Entry(entityToDelete).State == EntityState.Detached)
+                {
+                    dataSet.Attach(entityToDelete);
+                }
+                dataSet.Remove(entityToDelete);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return dataSet.ToList();
+        }
+
+        public T Find(object id)
+        {
+            return dataSet.SingleOrDefault(m => m.Id.Equals(id));
+        }
+
+        public bool Exists(object Id)
+        {
+            return dataSet.Any(b => b.Id.Equals(Id));
+        }
+
+        public List<T> FindWithPagedSearch(string query)
+        {
+            return dataSet.FromSql<T>(query).ToList();
+        }
+
+        public List<T> FindWithPagedSearch(string query, object[] parameters)
+        {
+            return dataSet.FromSql<T>(query, parameters).ToList();
+        }
+
+        public int GetCount(string query)
+        {
+            return dataSet.FromSql(query).Count();
+        }
+    }
+}
---
+++ SimpleRestfulAPIWithAspNetCore-v06 - Generic Repository/SimpleRestfulAPIWithAspNetCore/Repository/Generic/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using SimpleRestfulAPIWithAspNetCore.Models.Entities.Base;
+using System.Collections.Generic;
+
+namespace SimpleRestfulAPIWithAspNetCore.Repository.Generic
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Add(T item);
+        T Update(T item);
+        void Remove(object Id);
+        void Remove(T item);
+        IEnumerable<T> FindAll();
+        T Find(object Id);
+        bool Exists(object Id);
+
+        List<T> FindWithPagedSearch(string query);
+        List<T> FindWithPagedSearch(string query, object[] parameters);
+        int GetCount(string query);
+    }
+}
