3fb4ffc3e4c2d979839d555bedd9086319536ac2
leandrocgsi <leandrocgsi@gmail.com>
2018-05-04 09:12:35 -0300

    Implementing REST calculator

---
+++ SimpleRestfulAPIWithAspNetCore-v00 - Scafold/SimpleRestfulAPIWithAspNetCore/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleRestfulAPIWithAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            return Created($"api/Values/{value}", value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return Accepted(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
